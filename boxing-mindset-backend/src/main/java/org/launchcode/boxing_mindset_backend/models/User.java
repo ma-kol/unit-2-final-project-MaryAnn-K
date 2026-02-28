a5b8c3ba483c77830255655c0f89c9b54d9324c1
MA <171463891+ma-kol@users.noreply.github.com>
2026-02-28 00:14:04 -0500

    Created models and controllers packages; added User.java class to models

---
+++ boxing-mindset-backend/src/main/java/org/launchcode/boxing_mindset_backend/models/User.java
@@ -0,0 +1,79 @@
+package org.launchcode.boxing_mindset_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.OffsetDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String email;
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    // Default constructor for DB
+    public User() {
+    }
+
+    ;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    // TODO: Come back and look at these Override methods
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        return super.equals(obj);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return super.hashCode();
+//    }
+}
