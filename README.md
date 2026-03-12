1877d534dd4161e8c5742369f4429b731e504716
MA <171463891+ma-kol@users.noreply.github.com>
2026-03-11 22:01:09 -0400

    Fixing repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# unit-2-final-project-MaryAnn-K
-LaunchCode Unit 2 Final Project - Boxing Mindset (Fullstack Application)
--- boxing-mindset-backend/src/main/java/org/launchcode/boxing_mindset_backend/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.boxing_mindset_backend.repositories;
-
-import org.launchcode.boxing_mindset_backend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
